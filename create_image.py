da626212bfe58bc3f7daa1e49acd2e3d678fd280
evgen <evgeny.lipin@yandex.ru>
2023-09-22 16:28:17 +0300

    creat v2 with generetion of iamges

--- create_image.py
+++ create_image.py
@@ -1,63 +1,54 @@
-from urllib.parse import quote
-import requests
-from bs4 import BeautifulSoup
-import shutil
-from icrawler.builtin import GoogleImageCrawler
-from PIL import Image, UnidentifiedImageError
+from PIL import Image
 import os
+import freeGPT
+from io import BytesIO
+from asyncio import run, gather
+import shutil
+root_dir_images = f'{os.getcwd()}/data/images'
 
-root_dir_images = os.getcwd() + '/images'
 
-def get_images_from_istockphoto(word, headers):
-    base_pic_url = "https://www.istockphoto.com/ru/search/2/image?phrase="
-    url_encoded_string = quote(word)
-    pic_full_url = base_pic_url + url_encoded_string
+# asynchronous function for creating and saving image generation
+async def get_images(prompt, file_name):
+    try:
+        resp = await getattr(freeGPT, "prodia").Generation().create(prompt)
+        with Image.open(BytesIO(resp)) as image:
+            image.save(f'{file_name}.png')
+    except Exception as e:
+        print(f"Generation Image Error: {e}\n")
 
-    response_pic = requests.get(pic_full_url, headers=headers)
-    soup = BeautifulSoup(response_pic.text, "lxml") #html.parser
-    data = soup.find("div", class_ = 'DE6jTiCmOG8IPNVbM7pJ').find_all('img')
-
-    image_urls = [image['src'] for image in data]
+# asynchronous function for creating list of commands
+async def get_images_prodia(word, definition, example_sentence, example_sentence_2,  example_sentence_3): 
+    prompts = [
+        f'{definition}({word})',
+        f'{example_sentence}',
+        f'{example_sentence_2}',
+        f'{example_sentence_3}'
+    ]
 
     if not os.path.exists(root_dir_images):
         os.makedirs(root_dir_images)
 
-    count = 0
-    for image_url in image_urls[:8:2]:
-        count += 1
-        response = requests.get(image_url, stream=True)
-        with open(root_dir_images+ '/' + word + str(count) + '.jpg', 'wb') as out_file:
-            shutil.copyfileobj(response.raw, out_file)
+    tasks = []
+    for i, prompt in enumerate(prompts, 1):
+        file_name = f'{root_dir_images}/{word}{str(i)}'
+        tasks.append(get_images(prompt, file_name))
+    
+    await gather(*tasks)
 
-def create_collage(word):
+# creating collage from 4 gereratic pictures
+def create_collage(root_dir_data,word):
     collage = Image.new('RGB', (1600, 400))
     positions_images = [(0,0),(400,0),(800,0),(1200,0)]
     for i in range(1,5):
-        image_original = Image.open(root_dir_images + '/' + word + str(i) + '.jpg')
+        image_original = Image.open(f'{root_dir_images}/{word}{str(i)}.png')
         width, height = image_original.size
         new_width = 400
         new_height = int(400 * height / width)
         image_resize = image_original.resize((new_width, new_height))
         collage.paste(image_resize, positions_images[i-1])
-    collage.save('collage_' + word + '.jpg')
+    collage.save(f'{root_dir_data}/collage_{word}.jpg')
 
-def delete_images(word):
-    files = os.listdir(root_dir_images)
-    for file in files:
-        if (file.endswith('.jpg')) or (file.endswith('.png')):
-            os.remove(os.path.join(root_dir_images, file))
-    os.removedirs(root_dir_images)
-
-def create_image_collage(word, headers):
-    get_images_from_istockphoto(word, headers)
-    try:
-        create_collage(word)
-    except UnidentifiedImageError:
-        delete_images(word)
-        return False
-    
-    delete_images(word)
-    return True
-
-if __name__ == "__main__":
-    create_image_collage()
+def create_image_collage(root_dir_data, word, definition, example_sentence, example_sentence_2, example_sentence_3):
+    run(get_images_prodia(word, definition, example_sentence, example_sentence_2, example_sentence_3))
+    create_collage(root_dir_data, word)
+    shutil.rmtree(root_dir_images)
--- main.py
+++ main.py
@@ -2,30 +2,41 @@ import requests
 from bs4 import BeautifulSoup
 import genanki
 import os
-from clean import clean_all
 from create_image import create_image_collage
+import random
+import shutil
 
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0;Win64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'
+}
+root_dir_data = f'{os.getcwd()}/data'
+
+# cleaning "words.txt"
 def clean_words_txt():
     with open("words.txt", 'w', encoding='utf-8') as wfile:
         wfile.write('')
 
-def get_sound_word(sound_url, word, headers):
+# saving soundfile
+def get_sound_word(sound_url, word):
     response_sound = requests.get(sound_url, headers=headers)
-    with open(word + '.mp3', 'wb') as f:
+    with open(f'{root_dir_data}/{word}.mp3', 'wb') as f:
         f.write(response_sound.content)
-
-def parser_reverso(headers, reverso_dict_url, word):
-    word_url = reverso_dict_url + word
+    
+# parsing examles from reverso.net
+def parser_reverso(word):
+    word_url = f'https://context.reverso.net/translation/english-russian/{word}'
     response_word = requests.get(word_url, headers=headers)
     soup = BeautifulSoup(response_word.text, "lxml") #html.parser
-    data = soup.find("section", id='examples-content')
-    data_example_sentence = data.find("div", class_="example")
-    example_sentence = data_example_sentence.find("div", class_= "src ltr").text.strip()
-    return example_sentence
+    examples = soup.find_all('div', class_='src ltr')
+    list_exaples = [] 
+    for example in examples:
+        example_sentence = example.text.strip()
+        list_exaples.append(example_sentence)
+    return random.choice(list_exaples)
 
-def perser_cambrige(headers, cambridge_base_url, cambridge_dict_url, word, reverso_dict_url):
-    print('The word "' + word + '" is loaded...')
-    word_url = cambridge_dict_url + word
+# parsing data of word from cambridge.org
+def perser_cambrige(word):
+    word_url = f'https://dictionary.cambridge.org/us/dictionary/english/{word}'
     response_word = requests.get(word_url, headers=headers)
     soup = BeautifulSoup(response_word.text, "lxml") #html.parser
     data = soup.find("div", class_ = 'pr entry-body__el')
@@ -35,23 +46,28 @@ def perser_cambrige(headers, cambridge_base_url, cambridge_dict_url, word, rever
     try:
         example_sentence = data.find("span", class_ = "eg deg").text
     except AttributeError:
-        example_sentence = parser_reverso(headers, reverso_dict_url, word)
+        example_sentence = parser_reverso(word)
     pronanciation = data.find("span", class_ = "ipa dipa lpr-2 lpl-1").text 
-    sound_url = cambridge_base_url + data.find("source", type = "audio/mpeg")['src']
+    sound_url = 'https://dictionary.cambridge.org/' + data.find("source", type = "audio/mpeg")['src']
 
-    get_sound_word(sound_url, word, headers)
+    get_sound_word(sound_url, word)
 
     return type_word, definition, example_sentence, pronanciation
 
+def process_word(word):
+    print(f'The word "{word}" is loading...')
+    try:
+        type_word, definition, example_sentence, pronanciation = perser_cambrige(word)
+    except AttributeError as e:
+        return None
+
+    example_sentence_2 = parser_reverso(word)
+    example_sentence_3 = parser_reverso(word)
+    create_image_collage(root_dir_data, word, definition, example_sentence, example_sentence_2, example_sentence_3)
+    
+    return type_word, definition, example_sentence, pronanciation
+
 def main():
-    cambridge_base_url = 'https://dictionary.cambridge.org'
-    cambridge_dict_url = 'https://dictionary.cambridge.org/us/dictionary/english/'
-
-    reverso_dict_url = 'https://context.reverso.net/translation/english-russian/'
-
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0;Win64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'
-    }
 
     my_model = genanki.Model(
     1686681943,
@@ -81,7 +97,7 @@ def main():
     problem_words = []
 
     if os.path.exists('words.txt') == False:
-        print('"words.txt" does not exist') 
+        print("'words.txt' doen't exist") 
     elif os.stat('words.txt').st_size == 0:
         print('"words.txt" list is empty')
     else:
@@ -90,39 +106,36 @@ def main():
         for word in lines:
             if word:
                 try:
-                    type_word, definition, example_sentence, pronanciation = perser_cambrige(headers, cambridge_base_url, cambridge_dict_url, word, reverso_dict_url)
-                except AttributeError:
-                    problem_words.append(word)
-                    print("Parsing Error\n")
-                    continue
+                    if not os.path.exists(root_dir_data):
+                        os.makedirs(root_dir_data)
 
-                if create_image_collage(word=word, headers=headers) == False:
+                    type_word, definition, example_sentence, pronanciation = process_word(word)
+                    print("Success\n")
+                except TypeError as e:
+                    print(f"Processing Error\n")
                     problem_words.append(word)
-                    print("Image Error\n")
-                    continue                    
+                    continue
 
                 my_note = genanki.Note(
                 model=my_model,
-                fields=[word, type_word, pronanciation, definition, example_sentence, "[sound:" + word + ".mp3]", "<img src='collage_" + word + ".jpg'>"])
+                fields=[word, type_word, pronanciation, definition, example_sentence, f"[sound:{root_dir_data}/{word}.mp3]", f"<img src='{root_dir_data}/collage_{word}.jpg'>"])
                 
                 my_deck.add_note(my_note)
-                my_package.media_files.append(word + '.mp3')
-                my_package.media_files.append('collage_' + word + '.jpg')
-                print("Success\n")
+                my_package.media_files.append(f'{root_dir_data}/{word}.mp3')
+                my_package.media_files.append(f'{root_dir_data}/collage_{word}.jpg')
 
         if problem_words:
             with open('problem_words.txt', 'a', encoding='utf-8') as afile:
                 for word in problem_words:
-                    afile.write(word + "\n")
+                    afile.write(f"{word}\n")
                 print('Creat "problem_words.txt"\n')
         else:
             print("Problem words did't find")
 
         my_package.write_to_file('output.apkg')
         clean_words_txt()
-        clean_all()
+        shutil.rmtree(root_dir_data)
         print("The program is finished")
     
 if __name__ == "__main__":
     main()
-
